443b8fe62477f285e5158793a8ac3b58f9edc564
Christian Dugenio <christiandugenio@gmail.com>
2017-11-20 19:54:55 -0800

    Add requirejs and requirejs/text; Inject stats into app

--- app/main.js
+++ app/main.js
@@ -1,26 +1,45 @@
-console.log("Hello world!")
-const gl = document.getElementById("glcanvas").getContext("webgl2");
-const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
+require.config({
+    baseUrl: "/app",
+    paths: {
+        // Note that text.js is symlinked in /app to make text! easier
+    },
+});
 
-const arrays = {
-    position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
-};
-const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+require(
+    ["text"],
+    function(text){
+        // inject stats
+        var stats;
+        stats =  new Stats();
+        stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+        document.body.appendChild( stats.dom );
 
-function render(time) {
-    twgl.resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        const gl = document.getElementById("glcanvas").getContext("webgl2");
+        const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
 
-    const uniforms = {
-        time: time * 0.001,
-        resolution: [gl.canvas.width, gl.canvas.height],
-    };
+        const arrays = {
+            position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+        };
+        const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
 
-    gl.useProgram(programInfo.program);
-    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-    twgl.setUniforms(programInfo, uniforms);
-    twgl.drawBufferInfo(gl, bufferInfo);
+        function render(time) {
+            stats.begin();
+            twgl.resizeCanvasToDisplaySize(gl.canvas);
+            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    requestAnimationFrame(render);
-}
-requestAnimationFrame(render);
+            const uniforms = {
+                time: time * 0.001,
+                resolution: [gl.canvas.width, gl.canvas.height],
+            };
+
+            gl.useProgram(programInfo.program);
+            twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+            twgl.setUniforms(programInfo, uniforms);
+            twgl.drawBufferInfo(gl, bufferInfo);
+            stats.end();
+
+            requestAnimationFrame(render);
+        }
+        requestAnimationFrame(render);
+    }
+);
