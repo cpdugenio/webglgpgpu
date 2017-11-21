3a5ba03f3b048cdd9bbe96a582ca6e22a49160b3
Christian Dugenio <christiandugenio@gmail.com>
2017-11-20 17:48:32 -0800

    Add twgl.js helloworld and Makefile

---
+++ app/main.js
@@ -0,0 +1,26 @@
+console.log("Hello world!")
+const gl = document.getElementById("glcanvas").getContext("webgl2");
+const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
+
+const arrays = {
+    position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+};
+const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+function render(time) {
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    const uniforms = {
+        time: time * 0.001,
+        resolution: [gl.canvas.width, gl.canvas.height],
+    };
+
+    gl.useProgram(programInfo.program);
+    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+    twgl.setUniforms(programInfo, uniforms);
+    twgl.drawBufferInfo(gl, bufferInfo);
+
+    requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
