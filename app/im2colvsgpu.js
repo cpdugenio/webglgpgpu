0504a77e9e22bb738d0b08553f1c65876bdfc8b6
Christian Dugenio <christiandugenio@gmail.com>
2017-11-26 17:58:36 -0800

    Add cat input - fix up 3D textures

--- app/im2colvsgpu.js
+++ app/im2colvsgpu.js
@@ -11,25 +11,53 @@ require(
         /* INJECT STATS */
         var stats;
         stats =  new Stats();
-        stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+        stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
         document.body.appendChild( stats.dom );
 
+        /* SETUP GL */
+        const gl = document.createElement("canvas").getContext("webgl2");
+        const canvasbody = document.getElementById("glcanvas");
+        const context2d = document.createElement("canvas").getContext("2d");
+
         /* PROPS */
-        const iwidth = 2;
-        const iheight = 2;
-        const idepth = 2;
-        const kwidth = 1;
-        const kheight = 1;
-        const kdepth = 2;
-        const kcount = 2;
+        const kwidth = 3;
+        const kheight = 3;
+        const kdepth = 3;
+        const kcount = 500;
+        const RGBA = 4;
+
+        // var canvas = document.createElement( 'canvas' );
+        // var context = canvas.getContext( '2d' );
+        // context.drawImage( image, w, h, -w, -h );
+
+        /* Get input image pixels */
+        var image = document.getElementById("input");
+        var iheight = image.height;
+        var iwidth = image.width;
+        var idepth = 3;
+        var ctx = canvasbody.getContext('2d');
+        canvasbody.width = iwidth;
+        canvasbody.height = iheight;
+        ctx.clearRect(0,0,iwidth,iheight)
+        ctx.drawImage(image, 0, 0, iwidth, iheight, 0, 0, iwidth, iheight)
+        /* inputdata_strided has RGBA (4) */
+        const inputdata_strided = new Float32Array(
+            canvasbody.getContext('2d').getImageData(0, 0, 32, 32).data
+        );
+        ctx.clearRect(0,0,iwidth,iheight)
+        iheight = 32;
+        iwidth = 32;
+        const inputdata = new Float32Array(iwidth*iheight*idepth);
+        for(var i=0; i<iwidth*iheight; i++){
+            inputdata[iwidth*iheight*0+i] = inputdata_strided[4*i+0];
+            inputdata[iwidth*iheight*1+i] = inputdata_strided[4*i+1];
+            inputdata[iwidth*iheight*2+i] = inputdata_strided[4*i+2];
+            // Don't need alpha channel -- there is none
+        }
+
         const owidth = iwidth - kwidth + 1;
         const oheight = iheight - kheight + 1;
         const odepth = kcount;
-        const RGBA = 4;
-
-        /* SETUP GL */
-        // const gl = document.getElementById("glcanvas").getContext("webgl2");
-        const gl = document.createElement("canvas").getContext("webgl2");
 
         /* For float buffers */
         var ext = (
@@ -76,12 +104,6 @@ require(
         const framebufferInfo2D = twgl.createFramebufferInfo(gl, framebufferAttachments, iwidth, iheight);
 
         /* Input 3D texture   (X, Y, Z) */
-        const inputdata = new Float32Array([
-            1, 2,
-            3, 4,
-            101, 102,
-            103, 104,
-        ]);
         const input3d = twgl.createTexture(gl, {
             target: gl.TEXTURE_3D,
             width: iwidth,
@@ -92,14 +114,10 @@ require(
             src: inputdata,
         });
 
-
         /* Kernel 3D texture  (A, B, Z*N) */
-        const kerneldata = new Float32Array([
-            1,
-            2,
-            3,
-            4,
-        ]);
+        const kerneldata = new Float32Array(kwidth*kheight*kdepth*kcount);
+        kerneldata.fill(1.0/(kwidth*kheight*3.0));
+        
         const kernel3d = twgl.createTexture(gl, {
             target: gl.TEXTURE_3D,
             width: kwidth,
@@ -110,43 +128,38 @@ require(
             src: kerneldata,
         });
 
-        /* Output 3D texture  (X-A+1, Y-B+1, N) */
-        render(0);
+        /* Target 3D texture  (X-A+1, Y-B+1, N) */
+        const target3d = twgl.createTexture(gl, {
+            target: gl.TEXTURE_3D,
+            width: iwidth-kwidth+1,
+            height: iheight-kheight+1,
+            depth: kcount,
+            internalFormat: gl.R32F,
+            type: gl.FLOAT,
+        });
+
+        twgl.bindFramebufferInfo(gl, framebufferInfo2D);
+        gl.useProgram(convolveProgram.program);
+        twgl.setBuffersAndAttributes(gl, convolveProgram, bufferInfo);
+
+        // render(0);
         function render(time){
             stats.begin();
 
-            // ensure we're drawing to canvas
-            twgl.bindFramebufferInfo(gl, framebufferInfo2D);
-
-            const uniforms = {
-                input3d: input3d,
-                kernel3d: kernel3d,
-                kernelindex: 1,
-            };
-
-            gl.useProgram(convolveProgram.program);
-            twgl.setBuffersAndAttributes(gl, convolveProgram, bufferInfo);
-            twgl.setUniforms(convolveProgram, uniforms);
-            twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_STRIP, 4);
-
-            var framebufferDump2D = new Float32Array(iwidth*iheight*RGBA);
-            gl.readPixels(0, 0, iwidth, iheight, gl.RGBA, gl.FLOAT, framebufferDump2D)
-            console.log(framebufferDump2D.filter(function (d, i) { return i % 4 == 0; }));
+            glsl_convolve();
 
             stats.end();
-            // requestAnimationFrame(render);
+            requestAnimationFrame(render);
         }
         // requestAnimationFrame(render);
 
 
         /* Convolve */
-
         function glsl_convolve_init(input, kernel){
             /* Initialize all the good stuff for glsl_convolve */
-
         }
 
-        function glsl_convolve(input, kernel){
+        function glsl_convolve(){
             /* Returns output convolution object with data, width,
              * height, dim
              *
@@ -156,7 +169,49 @@ require(
              * Assumes glsl_convolve_init() has been run
              *
              */
+            for(var d=0; d<kcount; d++)
+            {
+                // TOOD: SELECT CORRECT TARGET TEXTURE Z-SLICE
+                const uniforms = {
+                    input3d: input3d,
+                    kernel3d: kernel3d,
+                    kernelindex: d,
+                };
+                twgl.setUniforms(convolveProgram, uniforms);
+                twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_STRIP, 4);
+
+                // var framebufferDump2D = new Float32Array(owidth*oheight*4);
+                // gl.readPixels(0, 0, owidth, oheight, gl.RGBA, gl.FLOAT, framebufferDump2D)
+                // var framebufferDump = new Float32Array(
+                //     framebufferDump2D.filter(function (d, i) { return i % 4 == 0; }));
+                // console.log(framebufferDump);
+
+                // var myImageData = ctx.createImageData(owidth, oheight);
+                // for(var i=0; i<framebufferDump.length; i++){
+                //     myImageData.data[i*4+0] = framebufferDump[i];
+                //     myImageData.data[i*4+1] = framebufferDump[i];
+                //     myImageData.data[i*4+2] = framebufferDump[i];
+                //     myImageData.data[i*4+3] = 255;
+                // }
+                // console.log(myImageData.data.length);
+                // console.log(framebufferDump.length*4);
+                // canvasbody.width = owidth;
+                // canvasbody.height = oheight;
+                // ctx.clearRect(0,0,owidth,oheight)
+                // ctx.putImageData(myImageData, 0, 0);
+            }
         }
+        var minny = 10000.0;
+        for(var x=0; x<100; x++){
+            var start = performance.now();
+            glsl_convolve();
+            var end = performance.now();
+            var duration = end - start;
+            if(duration < minny){
+                minny = duration;
+            }
+        }
+        console.log(minny);
 
         function im2col_convolve(input, kernel){
             /* Returns output convolution object with data, width,
