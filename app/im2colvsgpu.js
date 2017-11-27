1cb931f12bc40ad9e8a6d9c34f5f1ae7aee3d820
Christian Dugenio <christiandugenio@gmail.com>
2017-11-27 01:43:23 -0800

    Checkpoint

--- app/im2colvsgpu.js
+++ app/im2colvsgpu.js
@@ -20,10 +20,10 @@ require(
         const context2d = document.createElement("canvas").getContext("2d");
 
         /* PROPS */
-        const kwidth = 3;
-        const kheight = 3;
+        const kwidth = 20;
+        const kheight = 20;
         const kdepth = 3;
-        const kcount = 500;
+        const kcount = 1;
         const RGBA = 4;
 
         // var canvas = document.createElement( 'canvas' );
@@ -42,11 +42,12 @@ require(
         ctx.drawImage(image, 0, 0, iwidth, iheight, 0, 0, iwidth, iheight)
         /* inputdata_strided has RGBA (4) */
         const inputdata_strided = new Float32Array(
-            canvasbody.getContext('2d').getImageData(0, 0, 32, 32).data
+            // canvasbody.getContext('2d').getImageData(0, 0, 32, 32).data
+            canvasbody.getContext('2d').getImageData(0, 0, iwidth, iheight).data
         );
         ctx.clearRect(0,0,iwidth,iheight)
-        iheight = 32;
-        iwidth = 32;
+        // iheight = 32;
+        // iwidth = 32;
         const inputdata = new Float32Array(iwidth*iheight*idepth);
         for(var i=0; i<iwidth*iheight; i++){
             inputdata[iwidth*iheight*0+i] = inputdata_strided[4*i+0];
@@ -66,9 +67,9 @@ require(
 
         /* Augment fragment shader */
         convolve_fs = "#version 300 es\n"
-                      + "#define KU " + kwidth + "\n"
-                      + "#define KV " + kheight + "\n"
-                      + "#define KW " + kdepth + "\n"
+                      + "#define KU " + kwidth + ".0 \n"
+                      + "#define KV " + kheight + ".0 \n"
+                      + "#define KW " + kdepth + ".0 \n"
                       + "precision highp float;\n"
                       + "precision highp sampler3D;\n" + convolve_fs;
         const convolveProgram = twgl.createProgramInfo(gl, [convolve_vs, convolve_fs]);
@@ -109,6 +110,7 @@ require(
             width: iwidth,
             height: iheight,
             depth: idepth,
+            minMag: gl.NEAREST,
             internalFormat: gl.R32F,
             type: gl.FLOAT,
             src: inputdata,
@@ -117,12 +119,13 @@ require(
         /* Kernel 3D texture  (A, B, Z*N) */
         const kerneldata = new Float32Array(kwidth*kheight*kdepth*kcount);
         kerneldata.fill(1.0/(kwidth*kheight*3.0));
-        
+
         const kernel3d = twgl.createTexture(gl, {
             target: gl.TEXTURE_3D,
             width: kwidth,
             height: kheight,
             depth: kdepth*kcount,
+            minMag: gl.NEAREST,
             internalFormat: gl.R32F,
             type: gl.FLOAT,
             src: kerneldata,
@@ -134,8 +137,10 @@ require(
             width: iwidth-kwidth+1,
             height: iheight-kheight+1,
             depth: kcount,
+            minMag: gl.NEAREST,
             internalFormat: gl.R32F,
             type: gl.FLOAT,
+            data: null,
         });
 
         twgl.bindFramebufferInfo(gl, framebufferInfo2D);
@@ -160,18 +165,10 @@ require(
         }
 
         function glsl_convolve(){
-            /* Returns output convolution object with data, width,
-             * height, dim
-             *
-             * input: object with data, width, height, dim
-             * kernel: object with data, width, height, dim
-             *
-             * Assumes glsl_convolve_init() has been run
-             *
-             */
             for(var d=0; d<kcount; d++)
             {
                 // TOOD: SELECT CORRECT TARGET TEXTURE Z-SLICE
+                var start = performance.now();
                 const uniforms = {
                     input3d: input3d,
                     kernel3d: kernel3d,
@@ -179,50 +176,88 @@ require(
                 };
                 twgl.setUniforms(convolveProgram, uniforms);
                 twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_STRIP, 4);
+                var end = performance.now();
+                var duration = end - start;
+                console.log(duration);
 
-                // var framebufferDump2D = new Float32Array(owidth*oheight*4);
-                // gl.readPixels(0, 0, owidth, oheight, gl.RGBA, gl.FLOAT, framebufferDump2D)
-                // var framebufferDump = new Float32Array(
-                //     framebufferDump2D.filter(function (d, i) { return i % 4 == 0; }));
-                // console.log(framebufferDump);
+                var framebufferDump2D = new Float32Array(owidth*oheight*4);
+                gl.readPixels(0, 0, owidth, oheight, gl.RGBA, gl.FLOAT, framebufferDump2D)
+                var framebufferDump = new Float32Array(
+                    framebufferDump2D.filter(function (d, i) { return i % 4 == 0; }));
+                console.log(framebufferDump);
 
-                // var myImageData = ctx.createImageData(owidth, oheight);
-                // for(var i=0; i<framebufferDump.length; i++){
-                //     myImageData.data[i*4+0] = framebufferDump[i];
-                //     myImageData.data[i*4+1] = framebufferDump[i];
-                //     myImageData.data[i*4+2] = framebufferDump[i];
-                //     myImageData.data[i*4+3] = 255;
-                // }
-                // console.log(myImageData.data.length);
-                // console.log(framebufferDump.length*4);
-                // canvasbody.width = owidth;
-                // canvasbody.height = oheight;
-                // ctx.clearRect(0,0,owidth,oheight)
-                // ctx.putImageData(myImageData, 0, 0);
+                var myImageData = ctx.createImageData(owidth, oheight);
+                for(var i=0; i<framebufferDump.length; i++){
+                    myImageData.data[i*4+0] = framebufferDump[i];
+                    myImageData.data[i*4+1] = framebufferDump[i];
+                    myImageData.data[i*4+2] = framebufferDump[i];
+                    myImageData.data[i*4+3] = 255;
+                }
+                console.log(myImageData.data.length);
+                console.log(framebufferDump.length*4);
+                canvasbody.width = owidth;
+                canvasbody.height = oheight;
+                ctx.clearRect(0,0,owidth,oheight)
+                ctx.putImageData(myImageData, 0, 0);
             }
         }
-        var minny = 10000.0;
-        for(var x=0; x<100; x++){
-            var start = performance.now();
-            glsl_convolve();
-            var end = performance.now();
-            var duration = end - start;
-            if(duration < minny){
-                minny = duration;
+
+        function columnize(input_data, input_w, input_h, kernel_w) {
+            var columnized = new Uint8Array(
+                kernel_w * input_h * (input_w - kernel_w + 1) );
+            var i = -1;
+            for(var col=0; col<(input_w - kernel_w + 1); col++){
+                for(var h=0; h<input_h; h++){
+                    var off = col+h*input_w;
+                    for(var k=0; k<kernel_w; k++){
+                        columnized[++i] = input_data.data[off+k];
+                    }
+                }
+            }
+            return columnized;
+        };
+
+        var d, u, v, ku, kv, w;
+        function im2col_convolve(){
+            for(d=0; d<kcount; d++){
+                for(u=0; u<owidth; u++){
+                    for(v=0; v<oheight; v++){
+                        for(ku=0; ku<kwidth; ku++){
+                            for(kv=0; kv<kheight; kv++){
+                                for(w=0; w<kdepth; w++){
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        console.log(minny);
 
-        function im2col_convolve(input, kernel){
-            /* Returns output convolution object with data, width,
-             * height, dim
-             *
-             * input: object with data, width, height, dim
-             * kernel: object with data, width, height, dim
-             *
-             */
+        glsl_convolve();
 
-        }
+        // var minny = 10000.0;
+        // var maxy = 0.0;
+        // var s = 0.0;
+        // var loops = 10;
+        // for(var x=0; x<loops; x++){
+        //     var start = performance.now();
+        //     glsl_convolve();
+        //     var end = performance.now();
+        //     var duration = end - start;
+        //     if(duration < minny){
+        //         minny = duration;
+        //     } else if (duration > maxy){
+        //         maxy = duration;
+        //     }
+        //     s += duration;
+        // }
+        // console.log("glsl:");
+        // console.log(minny);
+        // console.log(maxy);
+        // console.log(s/loops);
+        // var div = document.createElement('div');
+        // div.innerHTML = minny + ' / ' + maxy + ' / ' + (s/loops);
+        // document.body.append(div);
     }
 );
 
--- app/shaders/convolve.fs
+++ app/shaders/convolve.fs
@@ -1,21 +1,26 @@
-// Assume KU, KV, KW defined
+// Assume KU, KV, KW (floats) defined
 uniform sampler3D input3d;
 uniform sampler3D kernel3d;
-uniform int kernelindex;
+uniform float kernelindex;
 
 in vec2 f_uv;
 out vec4 frag_color;
 
 void main() {
     vec4 color = vec4(0.0);
-    for(int x=0; x<KU; x++){
-        for(int y=0; y<KV; y++){
-            for(int z=0; z<KW; z++){
+    for(float x=0.0; x<KU; x++){
+        for(float y=0.0; y<KV; y++){
+            for(float z=0.0; z<KW; z++){
+                /*
                 ivec4 sk = ivec4(y, x, z+kernelindex*KW, 0);
                 ivec3 si = ivec3(round(f_uv.yx), z) + sk.xyw;
                 vec4 ifetch = texelFetch(input3d, si, 0);
                 vec4 kfetch = texelFetch(kernel3d, sk.xyz, 0);
                 color += ifetch * kfetch;
+                */
+                color +=
+                    texelFetch(input3d, ivec3(round(f_uv.y + y), round(f_uv.x + x), z), 0) *
+                    texelFetch(kernel3d, ivec3(y, x, z+kernelindex*KW), 0);
             }
         }
     }
